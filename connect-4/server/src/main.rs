da1b668edd843c8e5bf5d3e6135e8c5312ea7edd
raysarinas <raysarinas97@gmail.com>
2020-04-05 02:14:57 -0600

    mongonna kms

--- connect-4/server/src/main.rs
+++ connect-4/server/src/main.rs
@@ -8,6 +8,19 @@ use std::io;
 use std::path::{Path, PathBuf};
 
 use rocket::response::NamedFile;
+use bson::{Bson, doc, oid::ObjectId};
+use mongodb::{options::ClientOptions, Client, error::Error};
+
+pub fn connect_to_db() -> Result<mongodb::Client, mongodb::error::Error> {
+    // Parse a connection string into an options struct.
+    let mut client_options = ClientOptions::parse("mongodb://localhost:27017")?;
+
+    client_options.app_name = Some("Connect4".to_string());
+
+    // Get a handle to the deployment.
+    // Create the client
+    Client::with_options(client_options)
+}
 
 #[get("/")]
 fn index() -> io::Result<NamedFile> {
@@ -20,8 +33,24 @@ fn files(file: PathBuf) -> Option<NamedFile> {
 }
 
 #[get("/games")]
-fn games() -> &'static str {
-    "Hello, world!"
+fn games() -> String {
+    // format!("HELLO?")
+    let client = connect_to_db().unwrap();
+    let db = client.database("Connect4DB");
+    let coll = db.collection("games");
+    let mut cursor = coll
+                    .find(Some(doc! {}), None)
+                    .ok()
+                    .expect("Failed to execute find.");
+
+    let item = cursor.next();
+    match item {
+        Some(Ok(doc)) => {
+          println!("{:?}", doc);
+          format!("{:?}", doc)
+        }
+        _ => format!("Uh..."),
+      }
     // get request find Game() entry from MongoDB
     // handle error
     // post request and get entry and return it?
@@ -32,24 +61,24 @@ fn create_game() {
 }
 
 #[put("/games/<id>")]
-fn update_game(id: usize) {
+fn update_game(id: String) {
 
 }
 
 #[get("/games/<id>")]
-fn get_game(id: usize) {
+fn get_game(id: String) {
 
 }
 
 #[delete("/games/<id>")]
-fn delete_game(id: usize) {
+fn delete_game(id: String) {
 
 }
 
 fn get_routes() -> Vec<rocket::Route> {
     routes![
         index,
-        files//,games
+        files, games
     ]
 }
 
