230e9ef549469afba4891da055d47e8ac98f45f8
raysarinas <raysarinas97@gmail.com>
2020-04-08 18:57:41 -0600

    display game history 🌲🥭🚀

--- connect-4/client/src/routes/gamehistory.rs
+++ connect-4/client/src/routes/gamehistory.rs
@@ -1,16 +1,64 @@
-use yew::prelude::*;
+#![recursion_limit = "256"]
 
-pub struct GameHistory {}
+use yew::prelude::*;
+use serde_json::json;
+use yew::format::{Json, Nothing};
+use yew::services::fetch::{FetchService, FetchTask, Request, Response};
+use anyhow::Error;
+
+pub struct GameHistory {
+    link: ComponentLink<Self>,
+    ft: Option<FetchTask>,
+    state: State,
+}
+
+pub struct State {
+    link: ComponentLink<GameHistory>,
+    fetching: bool, // for when the server says "one moment please"
+    json_value: Option<String>, // TODO: replace String with Game struct
+}
+
+pub enum Msg {
+    Fetch,
+    FetchComplete(Result<String, Error>), // TODO: replace string with Game and refactor
+    FetchFailed,
+}
 
 impl Component for GameHistory {
-    type Message = ();
+    type Message = Msg;
     type Properties = ();
 
-    fn create(_: Self::Properties, _: ComponentLink<Self>) -> Self {
-        GameHistory {}
+    fn create(_: Self::Properties, link: ComponentLink<Self>) -> Self {
+        let mut state = State {
+            link: link.clone(),
+            fetching: true,
+            json_value: None,
+        };
+
+        // fetch JSON at the start
+        let task = state.get_history();
+        Self {
+            link,
+            ft: Some(task),
+            state,
+        }
     }
 
-    fn update(&mut self, _: Self::Message) -> ShouldRender {
+    fn update(&mut self, msg: Self::Message) -> ShouldRender {
+        match msg {
+            Msg::Fetch => {
+                let task = self.state.get_history();
+                self.ft = Some(task);
+            },
+            Msg::FetchComplete(body) => {
+                self.state.fetching = false;
+                self.state.json_value = body.map(|data| data).ok();
+            },
+            Msg::FetchFailed => {
+                self.state.json_value = None;
+                return false;
+            }
+        }
         true
     }
     
@@ -19,6 +67,9 @@ impl Component for GameHistory {
         html! {
             <div>
                 <h1><b>{"Game History"}</b></h1>
+                <div>
+                    <h4>{ self.view_history() }</h4>
+                </div>
                 <div>
                     <table border="1">
                         <tr>
@@ -36,5 +87,41 @@ impl Component for GameHistory {
                 </div>
             </div>
         }
+    }
+}
+
+impl GameHistory {
+    
+    fn view_history(&self) -> Html {
+        if let Some(value) = &self.state.json_value {
+            html! {
+                <>
+                // TODO: format game history i guess
+                { value }
+                </>
+            }
+        } else {
+            html! {
+                <p>{ "spare fetch?" }</p>
+            }
+        }
+    }
+}
+
+impl State {
+    fn get_history(&mut self) -> FetchTask {
+        let get_request = Request::get("/games")
+                .body(Nothing).expect("Failed to build request");
+        
+        let callback = self.link.callback(
+            | response: Response<Json<Result<String, Error>>> | {
+                let (meta, Json(body)) = response.into_parts();
+                if meta.status.is_success() {
+                    return Msg::FetchComplete(body);
+                }
+                Msg::FetchFailed
+            },
+        );
+        FetchService::new().fetch(get_request, callback).unwrap()
     }
 }
