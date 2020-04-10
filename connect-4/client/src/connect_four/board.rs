ee3ea69fcb31be4c92df47d409b1d32b3a0f6c64
raysarinas <raysarinas97@gmail.com>
2020-04-09 23:44:24 -0600

    draw on click! 🤡

--- connect-4/client/src/connect_four/board.rs
+++ connect-4/client/src/connect_four/board.rs
@@ -1,36 +1,54 @@
 use yew::prelude::*;
 
-pub struct ConnectFourBoard {}
+pub struct ConnectFourBoard {
+    link: ComponentLink<Self>,
+    clicked: Vec<Vec<bool>>,
+}
+
+pub enum Msg {
+    Click(usize, usize),
+}
 
 impl Component for ConnectFourBoard {
-    type Message = ();
+    type Message = Msg;
     type Properties = ();
 
-    fn create(_: Self::Properties, _: ComponentLink<Self>) -> Self {
-        ConnectFourBoard {}
+    fn create(_: Self::Properties, link: ComponentLink<Self>) -> Self {
+        ConnectFourBoard {
+            link,
+            clicked: vec![vec![false; 7]; 6],
+        }
     }
 
-    fn update(&mut self, _: Self::Message) -> ShouldRender {
+    fn update(&mut self, msg: Self::Message) -> ShouldRender {
+
+        match msg {
+            Msg::Click(row, col) => { // pass in the COL to Pranav's code
+                self.clicked[row][col] = true; 
+            }
+        };
         true
     }
 
     fn view(&self) -> Html {
-        let col = || {
+        let col = |r, c| {
             html! {
-                <td class="board_column">{""}</td>
+                <td class="board_column", onclick=self.link.callback(move |_| Msg::Click(r, c)),>
+                <div hidden=!self.clicked[r][c]>{ "x" }</div>
+                </td>
             }
         };
 
-        let row = || {
+        let row = |r| {
             html! {
                 <tr>
-                    {col()}
-                    {col()}
-                    {col()}
-                    {col()}
-                    {col()}
-                    {col()}
-                    {col()}
+                    {col(r, 0)}
+                    {col(r, 1)}
+                    {col(r, 2)}
+                    {col(r, 3)}
+                    {col(r, 4)}
+                    {col(r, 5)}
+                    {col(r, 6)}
                 </tr>
             }
         };
@@ -38,12 +56,12 @@ impl Component for ConnectFourBoard {
         html! {
             <div>
                 <table class="board">
-                    {row()}
-                    {row()}
-                    {row()}
-                    {row()}
-                    {row()}
-                    {row()}
+                    {row(0)}
+                    {row(1)}
+                    {row(2)}
+                    {row(3)}
+                    {row(4)}
+                    {row(5)}
                 </table>
                 <br></br>
             </div>
