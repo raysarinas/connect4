0f226484f1bcf7ad34df8ec1198df148c254defc
Trishia Gatilogo <gatilogo@ualberta.ca>
2020-04-10 01:51:06 -0600

    add turn logic to toot otto

--- connect-4/client/src/game_elements.rs
+++ connect-4/client/src/game_elements.rs
@@ -1,8 +1,22 @@
 pub mod nav_bar;
 
-#[derive(Copy, Clone)]
+#[derive(PartialEq, Copy, Clone)]
 pub enum Difficulty {
     Easy,
     Medium,
     Hard
+}
+
+pub enum Turn {
+    First,
+    Second,
+}
+
+impl Turn {
+    pub fn next(&mut self) {
+        match self {
+            Turn::First => *self = Turn::Second,
+            Turn::Second => *self = Turn::First,
+        }
+    }
 }
